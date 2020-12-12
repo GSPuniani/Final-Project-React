d64b36693b73d86098413ac5c7738abdebdc941f
Gobind Puniani <gobindpuniani@gmail.com>
2020-12-11 23:57:25 -0800

    Organized files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './Components/Title/Title';
+import POPOSList from './Components/POPOSList/POPOSList';
+import Footer from './Components/Footer/Footer';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import About from './Components/About/About';
+import POPOSDetails from './Components/POPOSDetails/POPOSDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Title />
+        <Route exact path="/" component={POPOSList}/>
+        <Route path="/about" component={About}/>
+        <Route path="/details/:id" component={POPOSDetails}/>
+        <img src={logo} alt='React Logo'/>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
